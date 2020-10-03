2a5a3104b88eb47700caf5f09ca92e8ee38732f9
Sheema Parwaz <sheemaparwaz@gmail.com>
2020-10-04 00:18:58 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,87 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var wall1, wall2, wall3, wall1Body,wall2Body,wall3Body;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 200, 10,10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
+
+	wall1=createSprite(width/2,650,200,20);
+	wall2=createSprite(290,610,20,100);
+	wall3=createSprite(510,610,20,100);
+	wall1.shapeColor="red";
+	wall2.shapeColor="red";
+	wall3.shapeColor="red";
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, density:1});
+	World.add(world, packageBody);
+	
+	wall1Body=Bodies.rectangle(width/2,650,200,20,{isStatic:true});
+	World.add(world,wall1Body);
+
+	wall2Body=Bodies.rectangle(290,610,20,100,{isStatic:true});
+	World.add(world,wall2Body);
+
+	wall3Body=Bodies.rectangle(510,610,20,100,{isStatic:true});
+	World.add(world,wall3Body);
+
+	console.log(wall1Body.type)
+	
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  console.log(packageBody.position);
+  keyPressed();
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+    packageSprite.x= packageBody.position.x ;
+   packageSprite.y= packageBody.position.y ;
+ 
+  }
+}
+
+
+
